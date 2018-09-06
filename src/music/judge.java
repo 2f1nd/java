3e2e8463989ebc62cd471d18334d339bee7f275f
2find <83688239@qq.com>
2018-09-06 17:30:32 +0800

    复习更新代码

---
+++ src/music/judge.java
@@ -0,0 +1,15 @@
+package music;
+
+public class judge {
+	public static void main(String[] args) {
+		int a=9;
+		double b=9.9;
+		int c=(int)b;
+		String str1="love";
+		String str2="you";
+		System.out.println("a等于b:"+(a==b));
+		System.out.println("a等于c:"+(a==c));
+		System.out.println("a大于等于b:"+(a>=b));
+		System.out.println("str1等于str2:"+(str1==str2));
+	}
+}
